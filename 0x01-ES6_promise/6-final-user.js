9f8df3c0369e1a61d2d5dbcfd0353636e85f02c8
Muluken <mulukeneshetu70@gmail.com>
2024-10-02 23:39:09 +0300

    ES6 Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,15 @@
-import signUpUser from './4-user-promise';  
-import uploadPhoto from './5-photo-reject';  
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {  
-  return Promise.allSettled([  
-    signUpUser(firstName, lastName),  
-    uploadPhoto(fileName),  
-  ]).then((values) => {  
-    const arr = [];  
-    for (const item of values) {  
-      // Check if the promise was fulfilled  
-      if (item.status === 'fulfilled') {  
-        arr.push({ status: item.status, value: item.value });  
-      } else {  
-        // It was rejected, capture the reason  
-        arr.push({ status: item.status, value: item.reason });  
-      }  
-    }  
-    return arr;  
-  });  
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
+  });
 }
