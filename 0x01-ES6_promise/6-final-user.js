abf7283bf6e4357f0a9512e94ff2949c60d09c80
Muluken <mulukeneshetu70@gmail.com>
2024-10-03 10:27:53 +0300

    ES6 Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+/**
+ * Handles user signup and photo upload.
+ * @param {string} firstName - The user's first name.
+ * @param {string} lastName - The user's last name.
+ * @param {string} fileName - The name of the photo file.
+ * @returns {Promise<Array>} - A promise that resolves to an array of results.
+ */
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.all([userPromise, photoPromise]).then((results) => results.map((result) => ({
-    status: 'fulfilled',
-    value: result,
-  }))).catch((error) => [{
-    status: 'rejected',
-    value: error,
-  }]);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    return values.map((item) => ({
+      status: item.status,
+      value: item.status === 'fulfilled' ? item.value : item.reason,
+    }));
+  });
 }
