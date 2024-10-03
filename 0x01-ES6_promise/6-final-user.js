d1f70b2a1fe20895a77b5faaac1b822961cadc79
Muluken <mulukeneshetu70@gmail.com>
2024-10-03 10:52:51 +0300

    ES6 Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-/**
- * Handles user signup and photo upload.
- * @param {string} firstName - The user's first name.
- * @param {string} lastName - The user's last name.
- * @param {string} fileName - The name of the photo file.
- * @returns {Promise<Array>} - A promise that resolves to an array of results.
- */
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => 
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }))
-  );
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
+  });
 }
--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,6 +1,3 @@
-// 6-main.js
 import handleProfileSignup from './6-final-user';
 
-handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg")
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
