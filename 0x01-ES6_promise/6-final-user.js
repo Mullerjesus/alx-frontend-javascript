ded241fe60235be217267d271d379b287b172f44
Muluken <mulukeneshetu70@gmail.com>
2024-10-03 11:07:26 +0300

    ES6 Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,9 +6,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((values) => {
-    return values.map((item) => ({
-      status: item.status,
-      value: item.status === 'fulfilled' ? item.value : item.reason,
-    }));
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
   });
 }
